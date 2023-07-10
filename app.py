ccbce961f179821ac0a770fdce805262a18bde0a
sinhnguyen-sunny <capsicumascando@gmail.com>
2023-07-10 08:31:03 +0000

    edited streamlit for multiple requests

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
-import os
+
+import langchain
+langchain.verbose = False
+from htmlTemplates import css, bot_template, user_template
 from dotenv import load_dotenv
+
+# Set the Streamlit page configuration and CSS styles
+st.set_page_config(page_title="PDF Buddy", page_icon=":coffee:")
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #fce6ef;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.write(css, unsafe_allow_html=True)
+st.header("PDF Buddy :coffee:")
 
 
 def get_pdf_text(pdf_docs):
@@ -31,23 +48,30 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-
-def get_vectorstore(text_chunks):
-    # embeddings = OpenAIEmbeddings()
-    print("HAHA")
+@st.cache_resource
+def load_embeddings():
     model_name = "hkunlp/instructor-xl"
     model_kwargs = {'device': 'cpu'}
     embeddings  = HuggingFaceInstructEmbeddings(
                 model_name=model_name, model_kwargs=model_kwargs)
-    print("HAHA")
+    return embeddings
+
+embeddings = load_embeddings()
+
+
+def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+@st.cache_resource
+def load_llm():
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":218})
+    return llm
+
+# Load the model and store it as a global variable
+llm = load_llm()
 
 def get_conversation_chain(vectorstore):
-    # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":218})
-
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
@@ -73,25 +97,12 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="PDF Buddy", page_icon=":coffee:")
-    st.markdown(
-        """
-        <style>
-        body {
-            background-color: #fce6ef;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("PDF Buddy :coffee:")
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
